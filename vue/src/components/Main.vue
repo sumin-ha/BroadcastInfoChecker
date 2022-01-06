cdf05be589079c0777fda55905d8283e09431f87
sumin-ha <AristiaHope@gmail.com>
2022-01-07 04:13:17 +0900

    220107 Main.vue 설명 추가

--- vue/src/components/Main.vue
+++ vue/src/components/Main.vue
@@ -1,32 +1,32 @@
 <template>
-  <div>
-      <div class="q-pa-md">
-          test
-      </div>
+  <div class="flex flex-center">
+    <div style="width:100%; text-align:center">
+      <h4 class="doc-heading doc-h3">프로그램 소개</h4>
+    </div>
+    <q-page style="min-height:100%">
+      <p>
+        트위터에 올라오는 라디오, 생방송 정보 및 라이브 정보를 기록하고, <br />
+        해당 정보를 정기적으로 트위터에 올려줌으로서, <br />
+        방송 하는 것을 잊고 못보는 불상사를 방지하고자 만든 프로그램입니다.
+      </p>
+      <p>
+        트위터 계정명과 검색하고 싶은 키워드를 등록하고,<br />
+        등록한 정보를 사용해 트위터에 검색돌려,<br />
+        원하는 라디오, 생방송 정보 및 라이브 정보를 검색<br />
+        적절하게 정제한 정보를 DB에 등록을 하게 되면,<br />
+        DB에 등록 된 방송 정보들을 트위터에 자동으로 올려주게 됩니다.
+      </p>
+      <p>
+        추가 기능으로, 주기적으로 게시되는 트위터 계정에<br />
+        적당한 태그 키워드를 멘션하면, 해당하는 정보를 돌려주는 기능을<br />
+        추후 개발 기능으로서 예정하고 있습니다.
+      </p>
+    </q-page>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue'
-
 export default {
-setup () {
-    const selected = ref([])
-    const broadcastTitle = ref(null)
-    const broadcastContext = ref(null)
-    const broadcastTag = ref(null)
-    const broadcastDate = ref(null)
-    const source = ref(null)
-
-    return {
-      selected,
-      broadcastTitle,
-      broadcastContext,
-      broadcastTag,
-      broadcastDate,
-      source,
-    }
-  }
 }
 </script>
 
