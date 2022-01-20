2667328eb2c7f9572ffa9a29603cc6a0be0830b9
sumin-ha <AristiaHope@gmail.com>
2022-01-21 01:45:41 +0900

    220121 - build.gradle 에러 수정

--- src/main/java/com/broadcastinfochecker/sm/web/IndexController.java
+++ src/main/java/com/broadcastinfochecker/sm/web/IndexController.java
@@ -16,27 +16,27 @@ public class IndexController {
     // 목록 화면 불러오기 용 서비스
     private final ListLoadService listLoadService;
 
-    // 접속 메인 페이지
-    @GetMapping("/")
-    public String index() {
-        return "index";
-    }
+//    // 접속 메인 페이지
+//    @GetMapping("/")
+//    public String index() {
+//        return "index";
+//    }
 
-    // 알리미 추가 및 수정 화면 요청
-    @GetMapping("/menuInfoRegister")
-    public String menuInfoRegister(Model model) {
-        // 화면에 돌려주는 모델에 리스트 데이터를 담음
-        model.addAttribute("posts", listLoadService.getInfoRegisterList());
-        return "menuInfoRegister";
-    }
-
-    // 방송 정보를 취득하고 구글 캘린더 등록, 트위터 알림용 등록을 위한 데이터 정제 화면 요청
-    @GetMapping("/menuInfoGetList")
-    public String menuInfoGetList(Model model) {
-        // 화면에 돌려주는 모델에 리스트 데이터를 담음
-        model.addAttribute("posts", listLoadService.getInfoListTemp());
-        return "menuInfoGetList";
-    }
+//    // 알리미 추가 및 수정 화면 요청
+//    @GetMapping("/menuInfoRegister")
+//    public String menuInfoRegister(Model model) {
+//        // 화면에 돌려주는 모델에 리스트 데이터를 담음
+//        model.addAttribute("posts", listLoadService.getInfoRegisterList());
+//        return null;
+//    }
+//
+//    // 방송 정보를 취득하고 구글 캘린더 등록, 트위터 알림용 등록을 위한 데이터 정제 화면 요청
+//    @GetMapping("/menuInfoGetList")
+//    public String menuInfoGetList(Model model) {
+//        // 화면에 돌려주는 모델에 리스트 데이터를 담음
+//        model.addAttribute("posts", listLoadService.getInfoListTemp());
+//        return "menuInfoGetList";
+//    }
 
     // 트위터 알림용으로 정제한 내용을 확인하는 화면 요청
     @GetMapping("/menuInfoCheck")
