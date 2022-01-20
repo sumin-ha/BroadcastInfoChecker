0c7c778a97f076c19b41d3e62c6a6e87df193c9e
sumin-ha <AristiaHope@gmail.com>
2022-01-21 01:33:46 +0900

    220121 - build.gradle 에러 수정

--- src/test/java/com/broadcastinfochecker/sm/web/PostApiControllerTest.java
+++ src/test/java/com/broadcastinfochecker/sm/web/PostApiControllerTest.java
@@ -15,6 +15,7 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -41,10 +42,10 @@ public class PostApiControllerTest {
 
     private MockMvc mvc;
 
-    @Mock
+    @MockBean
     private TweetInfoRegisterRepository infoRegisterRepository;
 
-    @Mock
+    @MockBean
     private BroadcastInfoRepository broadcastInfoRepository;
 
 //    @After
